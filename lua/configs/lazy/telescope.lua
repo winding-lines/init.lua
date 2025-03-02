5af0e3902d3751b4a5f9de6178daab690ef7f1c8
Marius S <39998+winding-lines@users.noreply.github.com>
2025-03-02 07:59:48 -0800

    Use cmd-p for Telescope git_files (in addition to ctrl-p).

--- lua/configs/lazy/telescope.lua
+++ lua/configs/lazy/telescope.lua
@@ -12,6 +12,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<D-p>', builtin.git_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
