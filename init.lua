5c589e54aebad93e3a3e68d49b1f4ef8f9554476
Marius S <39998+winding-lines@users.noreply.github.com>
2025-03-03 06:23:32 -0800

    add some help

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
 require("configs")
+
+-- setup the help system
+vim.api.nvim_create_augroup("FiletypeHelp",
+        { clear = true } -- Deletes the group if it already exists
+)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*/doc/*.txt",
+    callback = function()
+        vim.bo.filetype = "help"
+    end,
+    group = "FiletypeHelp", -- The above content belongs to this group
+})
