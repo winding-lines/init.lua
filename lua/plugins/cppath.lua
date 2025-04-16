386433630d8721ccb3e79ac35b90f63068605f67
Marius Seritan <39998+winding-lines@users.noreply.github.com>
2025-04-16 12:42:58 +0300

    Add CpPath command

---
+++ lua/plugins/cppath.lua
@@ -0,0 +1,9 @@
+M = {}
+
+vim.api.nvim_create_user_command("CpPath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+return M
