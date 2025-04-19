e32c5d3b9c46130b6652c0a97ebe7d2208f15296
Marius Seritan <39998+winding-lines@users.noreply.github.com>
2025-04-18 18:34:01 -0700

    Back to cyberdream color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,14 @@
 return {
   {
-    -- "scottmckendry/cyberdream.nvim",
-    "sainnhe/gruvbox-material",
+    "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
   },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "cyberdream",
-      colorscheme = "gruvbox-material",
+      colorscheme = "cyberdream",
     },
   },
 }
