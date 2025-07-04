0f0572fb7ecbbe19d0057c58619c34c6fb3095b6
Marius Seritan <39998+winding-lines@users.noreply.github.com>
2025-07-04 09:27:08 -0700

    Mojo tab stops

---
+++ after/ftplugin/mojo.lua
@@ -0,0 +1,3 @@
+vim.opt_local.expandtab = true -- Convert tabs to spaces
+vim.opt_local.shiftwidth = 4 -- Use this number of spaces for indentation
+vim.opt_local.tabstop = 4 -- Insert 2 spaces for a tab
