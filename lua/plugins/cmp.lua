88e1e04ee03c30a7249fd548da52e905eb048ee6
Marius Seritan <39998+winding-lines@users.noreply.github.com>
2025-12-20 06:45:52 -0800

    Working config dec 20th

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+require("blink.cmp").setup({
+  keymap = {
+    preset = "super-tab",
+  },
+  completion = {
+    accept = {
+      auto_brackets = { enabled = false }, -- Adjust other settings as needed
+    },
+  },
+})
+
+return {}
