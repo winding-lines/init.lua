5ce9471246b16817675d3ac755414b1b6d9b3c48
Marius Seritan <39998+winding-lines@users.noreply.github.com>
2026-03-15 07:16:50 -0700

    Redid config to bypass nvim crash.

--- lua/plugins/cppath.lua
+++ lua/plugins/cppath.lua
@@ -6,4 +6,8 @@ vim.api.nvim_create_user_command("CpPath", function()
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
+vim.api.nvim_create_user_command("LocalHelp", function()
+  vim.cmd("e ~/.local/share/nvim/site/doc/")
+end, {})
+
 return M
